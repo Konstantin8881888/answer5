4bef1eb7c5f9ff38fe64b59279b65597af255706
Konstantin <kamagoroff@gmail.com>
2023-04-10 01:38:49 +0300

    Updated

--- src/main/java/com/example/answer5/StudentSessionFactory.java
+++ src/main/java/com/example/answer5/StudentSessionFactory.java
@@ -1,19 +1,26 @@
 package com.example.answer5;
 
-
-
 import com.example.answer5.entity.Student;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class StudentSessionFactory {
-    public StudentSessionFactory() {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Student.class)
+                    .buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
     }
 
     public static SessionFactory getSessionFactory() {
-        return new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
-                .buildSessionFactory();
+        return sessionFactory;
     }
 }
--- src/main/java/com/example/answer5/dao/StudentDAO.java
+++ src/main/java/com/example/answer5/dao/StudentDAO.java
@@ -1,14 +1,13 @@
 package com.example.answer5.dao;
 
-
 import com.example.answer5.StudentSessionFactory;
 import com.example.answer5.entity.Student;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
-public class StudentDAO
-{
+public class StudentDAO implements AutoCloseable{
     private final Session session;
 
     public StudentDAO() {
@@ -16,7 +15,9 @@ public class StudentDAO
     }
 
     public void persist(Student student) {
+        Transaction transaction = session.beginTransaction();
         session.save(student);
+        transaction.commit();
     }
 
     public Student findById(Integer id) {
@@ -24,25 +25,36 @@ public class StudentDAO
     }
 
     public void update(Student entity) {
+        Transaction transaction = session.beginTransaction();
         session.update(entity);
+        transaction.commit();
     }
 
     public void delete(Student entity) {
+        Transaction transaction = session.beginTransaction();
         session.delete(entity);
+        transaction.commit();
     }
 
     public List<Student> findAll() {
-        return (List<Student>)
-                session.createQuery("from Student").list();
-
+        return session.createQuery("from Student", Student.class).list();
     }
 
     public void deleteAll() {
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
         List<Student> entityList = findAll();
         for (Student entity : entityList) {
             session.delete(entity);
         }
-        session.getTransaction().commit();
+        transaction.commit();
+    }
+
+    @Override
+    public void close() throws Exception {
+        closeConnect();
+    }
+
+    public void closeConnect() {
+        session.close();
     }
 }
